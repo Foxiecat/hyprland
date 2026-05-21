267c01a530e19d9fb4fe8032662414d2cd6d9453
Danica Ellefsen Kvilhaug <foxiecat+programming@pm.me>
2026-05-21 07:31:09 +0200

    refactor(colors): changed noctalia colorscheme template for hyprland to lua

--- noctalia/noctalia-colors.lua
+++ noctalia/noctalia-colors.lua
@@ -1,9 +1,9 @@
-local primary = 0xffcba6f7
-local surface = 0xff1e1e2e
-local secondary = 0xfffab387
-local error = 0xfff38ba8
-local tertiary = 0xff94e2d5
-local surface_lowest = 0xff212232
+local primary = "rgba(7aa2f7bf)"
+local surface = "rgba(1a1b26bf)"
+local secondary = "rgb(bb9af7)"
+local error_color = "rgb(f7768e)"
+local tertiary = "rgb(9ece6a)"
+local surface_lowest = "rgb(1c1d2a)"
 
 hl.config({
 	general = {
@@ -17,7 +17,7 @@ hl.config({
 		col = {
 			border_active = secondary,
 			border_inactive = surface,
-			border_locked_active = error,
+			border_locked_active = error_color,
 			border_locked_inactive = surface,
 		},
 
@@ -25,7 +25,7 @@ hl.config({
 			col = {
 				active = secondary,
 				inactive = surface,
-				locked_active = error,
+				locked_active = error_color,
 				locked_inactive = surface,
 			},
 		},
