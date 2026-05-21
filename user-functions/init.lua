4f44e02c3736e7e5ceebacc69dba8ee08409d9dd
Danica Ellefsen Kvilhaug <foxiecat+programming@pm.me>
2026-05-21 07:32:00 +0200

    feat: started on adding user-functions to load custom added function modules

---
+++ user-functions/init.lua
@@ -0,0 +1,19 @@
+---@module 'user-functions'
+
+local user = {}
+
+local function load_module(name)
+	local ok, module = pcall(require, "user-functions." .. name)
+	if not ok then
+		print("Warning: Failed to load user-functions." .. name .. ": " .. tostring(module))
+	end
+	return module
+end
+
+-- =====================
+-- || Modules to load ||
+-- =====================
+
+-- TODO: Switch Layout function that enables and disables keybinds specifically for the layout being switched from
+
+return user
